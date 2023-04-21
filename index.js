f5142344ec7f66a60c2836e74f84ceadffde82f5
DharaniChinta <nvchinta@asu.edu>
2023-04-21 11:38:25 -0700

    query and mutation types

--- index.js
+++ index.js
@@ -8,19 +8,38 @@ app.get('/',(req,res)=>{
     res.send('GraphQL is amazing');
 });
 
+class Product {
+    constructor(id, {name,description,price,soldout,stores}){
+        this.id=id;
+        this.description=description;
+        this.price=price;
+        this.soldout=soldout;
+        this.stores=stores;
+    }
+}
+
+// basically add new products with my mutations in this one, and eventually we'll use persistence with a database
+const productDatabase = {};
+
 const root = { product: ()=>{
     return {
-        "id":2345678,
-        "name":"widget",
-        "description":"Beautiful widget to use in your garden",
-        "price":34.44,
-        "soldout":false,
-        "stores":[
-            {store:"TraderJoes"},
-            {store:"Safeway"}
-        ],
+            "id":2345678,
+            "name":"widget",
+            "description":"Beautiful widget to use in your garden",
+            "price":34.44,
+            "soldout":false,
+            "stores":[
+                {store:"TraderJoes"},
+                {store:"Safeway"}
+            ],
+        }
+    },
+    createProduct: ({input})=>{
+        let id = require('crypto').randomBytes(10).toString('hex');
+        productDatabase[id]=input;
+        return new Product(id,input);
     }
-}};
+};
 
 app.use('/graphql',graphqlHTTP({
     schema: schema,
