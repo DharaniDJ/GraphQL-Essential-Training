f10ffb6aba0e5cd9a7d38fcd47c0192b3c5776b1
DharaniChinta <nvchinta@asu.edu>
2023-04-21 12:35:27 -0700

    implemented resolver

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
+import resolvers from './resolvers';
 
 const app = express()
 
@@ -8,38 +9,26 @@ app.get('/',(req,res)=>{
     res.send('GraphQL is amazing');
 });
 
-class Product {
-    constructor(id, {name,description,price,soldout,stores}){
-        this.id=id;
-        this.description=description;
-        this.price=price;
-        this.soldout=soldout;
-        this.stores=stores;
-    }
-}
 
-// basically add new products with my mutations in this one, and eventually we'll use persistence with a database
-const productDatabase = {};
+const root = resolvers;
 
-const root = { product: ()=>{
-    return {
-            "id":2345678,
-            "name":"widget",
-            "description":"Beautiful widget to use in your garden",
-            "price":34.44,
-            "soldout":false,
-            "stores":[
-                {store:"TraderJoes"},
-                {store:"Safeway"}
-            ],
-        }
-    },
-    createProduct: ({input})=>{
-        let id = require('crypto').randomBytes(10).toString('hex');
-        productDatabase[id]=input;
-        return new Product(id,input);
-    }
-};
+// { product: ()=>{
+//     return {
+//             "id":2345678,
+//             "name":"widget",
+//             "description":"Beautiful widget to use in your garden",
+//             "price":34.44,
+//             "soldout":false,
+//             "stores":[
+//                 {store:"TraderJoes"},
+//                 {store:"Safeway"}
+//             ],
+//         }
+//     },
+    
+// };
+
+// resolvers are the functions that respond to queries and mutations
 
 app.use('/graphql',graphqlHTTP({
     schema: schema,
